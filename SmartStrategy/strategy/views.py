b0374892116b773d6328fbd2cb7d3631d475a077
Lucas <lucas.marrie.21@ucl.ac.uk>
2023-02-25 20:33:29 +0000

    to do

--- SmartStrategy/strategy/views.py
+++ SmartStrategy/strategy/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 #create home view 
 def home_view(request):
     return render(request, 'home.html')
+
+def getJobs(request):
+    #fetch data from red cross API
+    pass
+
+def showJob(request):
+    user = request.user
+    #generate job details based on keywords
