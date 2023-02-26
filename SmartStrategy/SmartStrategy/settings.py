dd0dcb52d30a30f07486b78149d8df49d90f0fb4
matthewgomezcullen <zcabgom@ucl.ac.uk>
2023-02-26 11:55:33 +0000

    boo

--- SmartStrategy/SmartStrategy/settings.py
+++ SmartStrategy/SmartStrategy/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://smartvolunteers.azurewebsites.net','https://*.127.0.0.1']
